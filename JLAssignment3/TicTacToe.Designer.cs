c12bc7437899bd72c80097405bd6111bb70a7b2f
John <jlane7590@yahoo.ca>
2020-10-18 19:41:40 -0400

    first commit

---
+++ JLAssignment3/TicTacToe.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace JLAssignment3
+{
+    partial class TicTacToe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbGame = new System.Windows.Forms.GroupBox();
+            this.pb9 = new System.Windows.Forms.PictureBox();
+            this.pb8 = new System.Windows.Forms.PictureBox();
+            this.pb7 = new System.Windows.Forms.PictureBox();
+            this.pb6 = new System.Windows.Forms.PictureBox();
+            this.pb5 = new System.Windows.Forms.PictureBox();
+            this.pb4 = new System.Windows.Forms.PictureBox();
+            this.pb3 = new System.Windows.Forms.PictureBox();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.gbGame.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbGame
+            // 
+            this.gbGame.Controls.Add(this.pb9);
+            this.gbGame.Controls.Add(this.pb8);
+            this.gbGame.Controls.Add(this.pb7);
+            this.gbGame.Controls.Add(this.pb6);
+            this.gbGame.Controls.Add(this.pb5);
+            this.gbGame.Controls.Add(this.pb4);
+            this.gbGame.Controls.Add(this.pb3);
+            this.gbGame.Controls.Add(this.pb2);
+            this.gbGame.Controls.Add(this.pb1);
+            this.gbGame.Location = new System.Drawing.Point(68, 33);
+            this.gbGame.Name = "gbGame";
+            this.gbGame.Size = new System.Drawing.Size(621, 591);
+            this.gbGame.TabIndex = 0;
+            this.gbGame.TabStop = false;
+            // 
+            // pb9
+            // 
+            this.pb9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb9.Location = new System.Drawing.Point(405, 386);
+            this.pb9.Name = "pb9";
+            this.pb9.Size = new System.Drawing.Size(175, 168);
+            this.pb9.TabIndex = 8;
+            this.pb9.TabStop = false;
+            this.pb9.Click += new System.EventHandler(this.ClickPictureBox);
+            // 
+            // pb8
+            // 
+            this.pb8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb8.Location = new System.Drawing.Point(224, 386);
+            this.pb8.Name = "pb8";
+            this.pb8.Size = new System.Drawing.Size(175, 168);
+            this.pb8.TabIndex = 7;
+            this.pb8.TabStop = false;
+            this.pb8.Click += new System.EventHandler(this.ClickPictureBox);
+            // 
+            // pb7
+            // 
+            this.pb7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb7.Location = new System.Drawing.Point(43, 386);
+            this.pb7.Name = "pb7";
+            this.pb7.Size = new System.Drawing.Size(175, 168);
+            this.pb7.TabIndex = 6;
+            this.pb7.TabStop = false;
+            this.pb7.Click += new System.EventHandler(this.ClickPictureBox);
+            // 
+            // pb6
+            // 
+            this.pb6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb6.Location = new System.Drawing.Point(405, 212);
+            this.pb6.Name = "pb6";
+            this.pb6.Size = new System.Drawing.Size(175, 168);
+            this.pb6.TabIndex = 5;
+            this.pb6.TabStop = false;
+            this.pb6.Click += new System.EventHandler(this.ClickPictureBox);
+            // 
+            // pb5
+            // 
+            this.pb5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb5.Location = new System.Drawing.Point(224, 212);
+            this.pb5.Name = "pb5";
+            this.pb5.Size = new System.Drawing.Size(175, 168);
+            this.pb5.TabIndex = 4;
+            this.pb5.TabStop = false;
+            this.pb5.Click += new System.EventHandler(this.ClickPictureBox);
+            // 
+            // pb4
+            // 
+            this.pb4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb4.Location = new System.Drawing.Point(43, 212);
+            this.pb4.Name = "pb4";
+            this.pb4.Size = new System.Drawing.Size(175, 168);
+            this.pb4.TabIndex = 3;
+            this.pb4.TabStop = false;
+            this.pb4.Click += new System.EventHandler(this.ClickPictureBox);
+            // 
+            // pb3
+            // 
+            this.pb3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb3.Location = new System.Drawing.Point(405, 38);
+            this.pb3.Name = "pb3";
+            this.pb3.Size = new System.Drawing.Size(175, 168);
+            this.pb3.TabIndex = 2;
+            this.pb3.TabStop = false;
+            this.pb3.Click += new System.EventHandler(this.ClickPictureBox);
+            // 
+            // pb2
+            // 
+            this.pb2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb2.Location = new System.Drawing.Point(224, 38);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(175, 168);
+            this.pb2.TabIndex = 1;
+            this.pb2.TabStop = false;
+            this.pb2.Click += new System.EventHandler(this.ClickPictureBox);
+            // 
+            // pb1
+            // 
+            this.pb1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb1.Location = new System.Drawing.Point(43, 38);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(175, 168);
+            this.pb1.TabIndex = 0;
+            this.pb1.TabStop = false;
+            this.pb1.Click += new System.EventHandler(this.ClickPictureBox);
+            // 
+            // TicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 660);
+            this.Controls.Add(this.gbGame);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "TicTacToe";
+            this.Text = "TicTacToe";
+            this.gbGame.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pb9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbGame;
+        private System.Windows.Forms.PictureBox pb9;
+        private System.Windows.Forms.PictureBox pb8;
+        private System.Windows.Forms.PictureBox pb7;
+        private System.Windows.Forms.PictureBox pb6;
+        private System.Windows.Forms.PictureBox pb5;
+        private System.Windows.Forms.PictureBox pb4;
+        private System.Windows.Forms.PictureBox pb3;
+        private System.Windows.Forms.PictureBox pb2;
+        private System.Windows.Forms.PictureBox pb1;
+    }
+}
+
---
+++ JLAssignment3/TicTacToe.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+/*
+    JLaneAssignment3
+    A C# version of the classic game Tic Tac Toe
+    Completed by:
+    John Lane, 2019 11 15
+ */
+namespace JLAssignment3
+{
+    /// <summary>
+    /// TicTacToe inherits from Form
+    /// </summary>
+    public partial class TicTacToe : Form
+    {
+        /// <summary>
+        /// Constructor initializes component 
+        /// and calls CreateGameBoard();
+        /// </summary>
+        public TicTacToe()
+        {
+            InitializeComponent();
+            CreateGameBoard();
+        }
+
+        private const byte ROWS = 3; //set rows in array as int constant
+        
+        private const byte COLUMNS = 3; //set columns in array as int constant
+        
+        private readonly Image redX = Properties.Resources.redX; //set red x image as global
+        
+        private readonly Image blue0 = Properties.Resources.blue0; //set blue 0 image as global
+       
+        private string x = "x"; //set a global and reassignable variable x to keep track of turns
+       
+        private int numberOfClicks = 0; //set number of clicks to 0
+
+        private PictureBox[,] pbArray = new PictureBox[ROWS, COLUMNS]; //create a 2D array of picture boxes
+        
+        /// <summary>
+        /// CreateGameBoard() assigns picturebox
+        /// objects to 2D array
+        /// </summary>
+        private void CreateGameBoard()
+        {
+            for (int i = 0; i < pbArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < pbArray.GetLength(1); j++)
+                {
+                    PictureBox pb = new PictureBox();
+                    pbArray[i, j] = pb;
+
+                }
+            }
+        }
+
+        /// <summary>
+        /// Picture boxes get cast with each click
+        /// and logic assigns images to picturbox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ClickPictureBox(object sender, EventArgs e)
+        {
+            PictureBox pb = (PictureBox)sender;
+
+            if (pb.Image == null && x == "x")
+            {
+                pb.SizeMode = PictureBoxSizeMode.Zoom;
+                pb.Image = redX; //assign image to picture box
+                x = "o"; //set the turn to 'o'
+                numberOfClicks++; //increment the number of clicks
+                AssignImage(pb); //send picture box to the array
+            }
+
+            if (pb.Image == null && x == "o")
+            {
+                pb.SizeMode = PictureBoxSizeMode.Zoom;
+                pb.Image = blue0;//assign image to picture box
+                x = "x";//set the turn to 'x'
+                numberOfClicks++; //increment the number of clicks
+                AssignImage(pb); //send picture box to the array
+            }
+
+            if (numberOfClicks >= 5) //victory occurs at 5 cicks or greater
+            {
+                CheckGameOver(); //if 5 or more clicks, check game method called. 
+            }
+        }
+        /// <summary>
+        /// Picture boxes with images assigned
+        /// are passed to a method and each picturebox
+        /// is given a spot on a 2D array
+        /// </summary>
+        /// <param name="pb"></param>
+        private void AssignImage(PictureBox pb) 
+        {
+            if (pb.Name == "pb1")
+            {
+                pbArray[0, 0].Image = pb.Image;
+            }
+
+            if (pb.Name == "pb2")
+            {
+                pbArray[0, 1].Image = pb.Image;
+            }
+
+            if (pb.Name == "pb3")
+            {
+                pbArray[0,2].Image = pb.Image;
+            }
+
+            if (pb.Name == "pb4")
+            {
+                pbArray[1,0].Image = pb.Image;
+            }
+
+            if (pb.Name == "pb5")
+            {
+                pbArray[1,1].Image = pb.Image;
+            }
+
+            if (pb.Name == "pb6")
+            {
+                pbArray[1,2].Image = pb.Image;
+            }
+
+            if (pb.Name == "pb7")
+            {
+                pbArray[2,0].Image = pb.Image;
+            }
+
+            if (pb.Name == "pb8")
+            {
+                pbArray[2,1].Image = pb.Image;
+            }
+
+            if (pb.Name == "pb9")
+            {
+                pbArray[2,2].Image = pb.Image;
+            }
+        }
+
+        /// <summary>
+        /// If any of the conditions are met below
+        /// it means X or O has won a vertical,
+        /// horizontal, or diagonal victory and 
+        /// GameOverMessage() called. If none are true
+        /// and there have been 9 clicks, it means a Tie
+        /// and TieMessage() called. 
+        /// </summary>
+        private void CheckGameOver() 
+        {
+            //check if x wins
+            if (pbArray[0,0].Image == redX && pbArray[1,0].Image == redX && pbArray[2,0].Image == redX)
+            {
+                GaveOverMessage("X");
+            }
+
+            if (pbArray[0, 1].Image == redX && pbArray[1, 1].Image == redX && pbArray[2, 1].Image == redX)
+            {
+                GaveOverMessage("X");
+            }
+
+            if (pbArray[0, 2].Image == redX && pbArray[1, 2].Image == redX && pbArray[2, 2].Image == redX)
+            {
+                GaveOverMessage("X");
+            }
+
+            if (pbArray[0, 0].Image == redX && pbArray[0, 1].Image == redX && pbArray[0, 2].Image == redX)
+            {
+                GaveOverMessage("X");
+            }
+
+            if (pbArray[1, 0].Image == redX && pbArray[1, 1].Image == redX && pbArray[1, 2].Image == redX)
+            {
+                GaveOverMessage("X");
+            }
+
+            if (pbArray[2, 0].Image == redX && pbArray[2, 1].Image == redX && pbArray[2, 2].Image == redX)
+            {
+                GaveOverMessage("X");
+            }
+
+            if (pbArray[0, 0].Image == redX && pbArray[1, 1].Image == redX && pbArray[2, 2].Image == redX)
+            {
+                GaveOverMessage("X");
+            }
+
+            if (pbArray[0, 2].Image == redX && pbArray[1, 1].Image == redX && pbArray[2, 0].Image == redX)
+            {
+                GaveOverMessage("X");
+            }
+            //check if 0 wins
+           
+            if (pbArray[0, 0].Image == blue0 && pbArray[1, 0].Image == blue0 && pbArray[2, 0].Image == blue0)
+            {
+                GaveOverMessage("O");
+            }
+
+            if (pbArray[0, 1].Image == blue0 && pbArray[1, 1].Image == blue0 && pbArray[2, 1].Image == blue0)
+            {
+                GaveOverMessage("O");
+            }
+
+            if (pbArray[0, 2].Image == blue0 && pbArray[1, 2].Image == blue0 && pbArray[2, 2].Image == blue0)
+            {
+                GaveOverMessage("O");
+            }
+
+            if (pbArray[0, 0].Image == blue0 && pbArray[0, 1].Image == blue0 && pbArray[0, 2].Image == blue0)
+            {
+                GaveOverMessage("O");
+            }
+
+            if (pbArray[1, 0].Image == blue0 && pbArray[1, 1].Image == blue0 && pbArray[1, 2].Image == blue0)
+            {
+                GaveOverMessage("O");
+            }
+
+            if (pbArray[2, 0].Image == blue0 && pbArray[2, 1].Image == blue0 && pbArray[2, 2].Image == blue0)
+            {
+                GaveOverMessage("O");
+            }
+
+            if (pbArray[0, 0].Image == blue0 && pbArray[1, 1].Image == blue0 && pbArray[2, 2].Image == blue0)
+            {
+                GaveOverMessage("O");
+            }
+
+            if (pbArray[0, 2].Image == blue0 && pbArray[1, 1].Image == blue0 && pbArray[2, 0].Image == blue0)
+            {
+                GaveOverMessage("O");
+            }
+
+            if (numberOfClicks==9) //none of prior conditions met, TieMessage() called.
+            {
+                TieMessage();
+            }  
+        }
+
+        /// <summary>
+        /// This method clears the images from 
+        /// the control and empties the 2D
+        /// picture box array
+        /// </summary>
+        /// <param name="winner"></param>
+        private void GaveOverMessage(string winner)
+        {
+          DialogResult dr= MessageBox.Show($"{winner} won!", "Game Over" , MessageBoxButtons.OK);
+
+            if (dr == DialogResult.OK)
+            {
+                for (int i = 0; i < gbGame.Controls.Count; i++)
+                {
+                    PictureBox pb = (PictureBox)gbGame.Controls[i];
+                    pb.Image = null;
+                }
+
+                for (int i = 0; i < pbArray.GetLength(0); i++)
+                {
+                    for (int j = 0; j < pbArray.GetLength(1); j++)
+                    {
+                        pbArray[i, j].Image = null;
+                    }
+                }
+
+                numberOfClicks = 0;
+            }
+        }
+
+        /// <summary>
+        /// This method just displays the "Tie" message
+        /// and clears the controls and 2D array of 
+        /// pictureboxes. 
+        /// </summary>
+        private void TieMessage() 
+        {
+            DialogResult dr = MessageBox.Show("Tie");
+
+            if (dr == DialogResult.OK)
+            {
+                for (int i = 0; i < gbGame.Controls.Count; i++)
+                {
+                    //this just clears the controls, not the array
+                    PictureBox pb = (PictureBox)gbGame.Controls[i]; 
+                    pb.Image = null;
+                }
+
+                for (int i = 0; i < pbArray.GetLength(0); i++)
+                {
+                    for (int j = 0; j < pbArray.GetLength(1); j++)
+                    {
+                        pbArray[i, j].Image = null;
+                    }
+                }
+
+                numberOfClicks = 0;
+            }
+        }
+    }
+}
